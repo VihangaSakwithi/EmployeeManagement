4852be1db2efb1cacaea94b454f5bde98dbd3d7b
Vihanga Sakwithi <vsakwithi@outlook.com>
2023-06-18 20:37:45 +0530

    Employee registration done

---
+++ EmployeeManagement/Controllers/EmployeesController.cs
@@ -0,0 +1,42 @@
+﻿using EmployeeManagement.Data;
+using EmployeeManagement.Models;
+using EmployeeManagement.Models.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagement.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private readonly DBContext empDBContext;
+
+        public EmployeesController(DBContext empDBContext)
+        {
+            this.empDBContext = empDBContext;
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(AddEmployeeView addEmployeeRequest)
+        {
+            var employee = new Employee()
+            {
+                Id = Guid.NewGuid(),
+                Name = addEmployeeRequest.Name,
+                Email = addEmployeeRequest.Email,
+                Salary = addEmployeeRequest.Salary,
+                DateOfBirth = addEmployeeRequest.DateOfBirth,
+                Department = addEmployeeRequest.Department,
+            };
+
+            empDBContext.Employees.Add(employee);
+            empDBContext.SaveChanges();
+
+            return RedirectToAction("Add");
+        }
+    }
+}
--- EmployeeManagement/Models/AddEmployeeView.cs
+++ EmployeeManagement/Models/AddEmployeeView.cs
@@ -1,8 +1,7 @@
-﻿namespace EmployeeManagement.Models.Domain
+﻿namespace EmployeeManagement.Models
 {
-    public class Employee
+    public class AddEmployeeView
     {
-        public Guid Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
         public long Salary { get; set; }
